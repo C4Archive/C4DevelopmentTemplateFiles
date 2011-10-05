d0695d02c8f0397100699542c5250fcef7951d37
C4 <tkirton@gmail.com>
2011-10-05 10:29:43 -0600

    project template works... 48hours later... an invisible ~/Library folder from Snow Leopard was making all the trouble
    Signed-off-by: C4 <tkirton@gmail.com>

--- C4 Mac Source Base.xctemplate/macSource/___PACKAGENAME___AppDelegate.h
+++ C4 Mac Source Base.xctemplate/macSource/___PACKAGENAME___AppDelegate.h
@@ -1,13 +1,14 @@
 //
-//  ___PACKAGENAME___AppDelegate.h
-//  ___PACKAGENAME___
+//  C4DevelopmentAppDelegate.h
+//  C4Development
 //
 //  Created by moi on 11-04-05.
 //  Copyright 2011 mediart. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
-#import "CFACanvas.h"
+#import "C4Canvas.h"
+#import "C4OpenGLView.h"
 
 #if (MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_5)
 @interface ___PACKAGENAME___AppDelegate : NSObject {
@@ -15,21 +16,8 @@
 	@interface ___PACKAGENAME___AppDelegate : NSObject <NSApplicationDelegate> {
 #endif
 		NSWindow *window;
-		IBOutlet CFACanvas *cfaCanvas;
-		
-	@private
-		CFAOpenGLView	*cfaOpenGLView;
-		CFAColor		*cfaColor;
-		CFADateTime		*cfaDateTime;
-		CFAFoundation	*cfaFoundation;
-		CFAMath			*cfaMath;
-		CFAShape		*cfaShape;
-		CFATransform	*cfaTransform;
-		CFAString		*cfaString;
-		CFAGlobalTypeAttributes	*cfaGlobalTypeAttributes;
-        CFAGlobalShapeAttributes *cfaGlobalShapeAttributes;
-        CFAGlobalStringAttributes *cfaGlobalStringAttributes;
-	}
+		IBOutlet C4Canvas *_C4Canvas;
+    }
     
     @property (assign) IBOutlet NSWindow *window;
 	
--- C4 Mac Source Base.xctemplate/macSource/___PACKAGENAME___AppDelegate.m
+++ C4 Mac Source Base.xctemplate/macSource/___PACKAGENAME___AppDelegate.m
@@ -1,6 +1,6 @@
 //
-//  ___PACKAGENAME___AppDelegate.m
-//  ___PACKAGENAME___
+//  C4DevelopmentAppDelegate.m
+//  C4Development
 //
 //  Created by moi on 11-04-05.
 //  Copyright 2011 mediart. All rights reserved.
@@ -12,24 +12,14 @@
 
 @synthesize window;
 +(void)load {
-	if(VERBOSELOAD) printf("___PACKAGENAME___AppDelegate\n");
+	if(VERBOSELOAD) printf("C4DevelopmentAppDelegate\n");
 }
 
 - (void)awakeFromNib {
-	/* create instances of singletons */
-	cfaOpenGLView	= [[[CFAOpenGLView alloc] _init] retain];
-	cfaDateTime		= [[[CFADateTime alloc] _init] retain];
-	cfaFoundation	= [[[CFAFoundation alloc] _init] retain];
-	cfaMath			= [[[CFAMath alloc] _init] retain];
-	cfaShape		= [[[CFAShape alloc] _init] retain];
-	cfaTransform	= [[[CFATransform alloc] _init] retain];
-	cfaGlobalTypeAttributes = [[[CFAGlobalTypeAttributes alloc] _init] retain];
-	cfaGlobalStringAttributes = [[[CFAGlobalStringAttributes alloc] _init] retain];
-	cfaGlobalShapeAttributes = [[[CFAGlobalShapeAttributes alloc] _init] retain];
 }
 
 -(void)applicationWillFinishLaunching:(NSNotification *)notification {
-	[cfaCanvas setupRect];
+	[_C4Canvas setupRect];
 }
 
 @end
